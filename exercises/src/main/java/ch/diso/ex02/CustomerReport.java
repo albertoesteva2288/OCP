af34d7cda06a00647c5909fc324a2d9acf286b86
Daniel D. Senften <dsenften@diso.ch>
2016-11-07 11:16:00 +0100

    Additional exercises added.

---
+++ exercises/src/main/java/ch/diso/ex02/CustomerReport.java
@@ -0,0 +1,35 @@
+package ch.diso.ex02;
+
+public class CustomerReport {
+
+    private Bank bank;
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public void generateReport() {
+
+        System.out.println("\t\t\tCUSTOMERS REPORT");
+        System.out.println("\t\t\t================");
+
+        for (int cust_idx = 0; cust_idx < bank.getNumOfCustomers(); cust_idx++) {
+            Customer customer = bank.getCustomer(cust_idx);
+
+            System.out.println();
+            System.out.println("Customer: "
+                    + customer.getLastName() + ", "
+                    + customer.getFirstName());
+
+            for (int acct_idx = 0; acct_idx < customer.getNumOfAccounts(); acct_idx++) {
+                TimeDepositAccount account = customer.getAccount(acct_idx);
+
+                System.out.println("    " + account);
+            }
+        }
+    }
+}
