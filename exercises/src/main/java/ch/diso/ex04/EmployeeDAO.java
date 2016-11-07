fdeee05c4cb2ada182fb77e64b4a385d6dd96269
Daniel D. Senften <dsenften@diso.ch>
2016-11-07 14:22:37 +0100

    Additional exercises added.

---
+++ exercises/src/main/java/ch/diso/ex04/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package ch.diso.ex04;
+
+public interface EmployeeDAO {
+
+    public void add(Employee emp);
+
+    public void update(Employee emp);
+
+    public void delete(int id);
+
+    public Employee findById(int id);
+
+    public Employee[] getAllEmployees();
+
+}
