182e12ad0ad75d7307217d4b21aa9eea2be9e39c
Daniel D. Senften <dsenften@diso.ch>
2016-11-07 14:00:05 +0100

    Additional examples added.

---
+++ exercises/src/main/java/ch/diso/ex03/Pet.java
@@ -0,0 +1,14 @@
+package ch.diso.ex03;
+
+/**
+ * User: Daniel D. Senften <dsenften@diso.ch>
+ * Date: 07.11.16, 13:56
+ */
+public interface Pet {
+
+    public String getName();
+
+    public void setName(String name);
+
+    public void play();
+}
