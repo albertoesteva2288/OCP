8bf04fa5b6f47dd5d77b53f39abaea955f463b16
Daniel D. Senften <dsenften@diso.ch>
2016-11-08 09:10:06 +0100

    Additional examples added.

---
+++ examples/src/main/java/ch/diso/ex04/UnboundedWildcard.java
@@ -0,0 +1,32 @@
+package ch.diso.ex04;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UnboundedWildcard {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 2, 3);
+        List<String> stringList = Arrays.asList("one", "two", "three");
+
+        printList(integerList);
+        printList(stringList);
+    }
+
+    /**
+     * The goal of printList is to print a list of any type. To write a
+     * generic printList method, use <code>List&lt;?&gt;</code>:
+     *
+     * @param list
+     *     List to be printed
+     *
+     * @see
+     * <a href="https://docs.oracle.com/javase/tutorial/java/generics/unboundedWildcards.html">Java Tutorial</a>
+     */
+     private static void printList(List<?> list) {
+        for (Object o : list) {
+            System.out.print(o + " ");
+        }
+        System.out.println();
+    }
+}
--- examples/src/main/java/ch/diso/ex21/MostPopularClient.java
+++ examples/src/main/java/ch/diso/ex21/MostPopularClient.java
@@ -1,4 +1,4 @@
-package ch.diso.collector;
+package ch.diso.ex21;
 
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
