8d09ee4766335a099cef53f7bb1da94df16c226b
Daniel D. Senften <dsenften@diso.ch>
2016-11-21 13:43:05 +0100

    Additional examples added.

---
+++ examples/src/main/java/ch/diso/ex14_Concurrent/LionCageManager.java
@@ -0,0 +1,40 @@
+package ch.diso.ex14_Concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LionCageManager {
+
+    private static final int COUNT = 10;
+
+    private void removeAnimals() {
+        System.out.println("Removing animals");
+    }
+
+
+    private void cleanCage() {
+        System.out.println("Cleaning the cage");
+    }
+
+    private void addAnimals() {
+        System.out.println("Adding animals");
+    }
+
+    public void performTask() {
+        removeAnimals();
+        cleanCage();
+        addAnimals();
+    }
+
+    public static void main(String[] args) {
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(COUNT);
+            LionCageManager manager = new LionCageManager();
+            for (int i = 0; i < COUNT; i++)
+                service.submit(() -> manager.performTask());
+        } finally {
+            if (service != null) service.shutdown();
+        }
+    }
+}
